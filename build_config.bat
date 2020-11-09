5522538349f661fc9e4e3f5aabf9a23f89dce943
Bruno Garcia <brwnow@gmail.com>
2020-11-08 23:05:51 -0300

    cleaning up local build configuration file

--- build_config.bat
+++ build_config.bat
@@ -1,9 +1,9 @@
 REM Path to bin folder of your toolchain
-set TOOLCHAIN_PATH=C:\MinGW\bin
+set TOOLCHAIN_PATH=
 
 REM Compiler within bin folder of your toolchain to be called for
 REM compliing and linking the project
-set C_COMPILER=gcc
+set C_COMPILER=
 
-REM Path to your installation of SDL2 lib containing bin, include and lib
-set LIBSDL_PATH=libs\sdl2
+REM Path to your installation of SDL2 lib containing bin, include and lib folders
+set LIBSDL_PATH=
