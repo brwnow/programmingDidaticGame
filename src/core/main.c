192d0a07842ab220e88c851febde232e2a94af74
Bruno Garcia <brwnow@gmail.com>
2020-11-09 03:01:21 -0300

    Updating short example in main function with some font drawing code

--- src/core/main.c
+++ src/core/main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 #include <SDL.h>
+#include <SDL_ttf.h>
 
 #include "utils/log.h"
 #include "core/video/video.h"
@@ -21,12 +22,51 @@ int main(int argc, char *argv[]) {
     if(windowStartup("Breno Game", 800, 600) == false)
         return 0;
 
+    if(TTF_Init() != 0) {
+        logCritical("Failed to initialize TTF SDL extension - reason %s", SDL_GetError());
+
+        return 0;
+    }
+
+    TTF_Font *font = TTF_OpenFont("res/fonts/AlexBrush-Regular.ttf", 80);
+    SDL_Surface *fontSurface = NULL;
+    SDL_Color fontColor = {145, 242, 145, 255};
+
+    if(font != NULL)
+        fontSurface = TTF_RenderText_Blended(font, "Eu sou o que eu quero ser!", fontColor);
+
+    TTF_CloseFont(font);
+    font = NULL;
+
+    SDL_Surface *screen = windowGetScreen();
+
     while(isKeyPressed(SDL_SCANCODE_ESCAPE) == false) {
         SDL_PumpEvents();
+
+        if(screen != NULL && fontSurface != NULL) {
+            int screenWidth = screen->w;
+            int screenHeight = screen->h;
+            int fontSurfWidth = fontSurface->w;
+            int fontSurfHeigth = fontSurface->h;
+            SDL_Rect destRect = {   (screenWidth - fontSurfWidth) / 2,
+                                    (screenHeight - fontSurfHeigth) / 2,
+                                    fontSurfWidth,
+                                    fontSurfHeigth };
+
+            SDL_BlitSurface(fontSurface, &(fontSurface->clip_rect), screen, &destRect);
+        }
+
+        windowFlipPage();
 
         SDL_Delay(25);
     }
 
+    SDL_FreeSurface(fontSurface);
+    SDL_FreeSurface(screen);
+    fontSurface = NULL;
+    screen = NULL;
+
+    TTF_Quit();
     windowClose();
     keyboardEnd();
     appEnd();
