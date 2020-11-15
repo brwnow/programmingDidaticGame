bc1ea1326b3fa317183e7044ebfe25f0677fe917
Bruno Garcia <brwnow@gmail.com>
2020-11-15 17:59:44 -0300

    Removing custom bool definition and adding stdbool.h

--- src/include/definitions/global.h
+++ src/include/definitions/global.h
@@ -1,20 +1,8 @@
 #ifndef _DIDATIC_GAME_DEFINITIONS_GLOBAL_H_
 #define _DIDATIC_GAME_DEFINITIONS_GLOBAL_H_
 
+#include <stdbool.h>
+
 #define UNUSED(x) (void)x
-
-#ifndef bool
-    #define bool int
-#endif // bool
-
-#ifndef true
-    #define true 1
-#endif // true
-
-#ifndef false
-    #define false 0
-#endif // false
-
-#define bool_cast(x) x ? true : false
 
 #endif // _DIDATIC_GAME_DEFINITIONS_GLOBAL_H_
