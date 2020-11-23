42ac292317c8095f30a08bdf047d5babbf8b7e64
Bruno Garcia <brwnow@gmail.com>
2020-11-22 21:13:47 -0300

    Updating build.bat to clean up and build tests artifacts

--- build.bat
+++ build.bat
@@ -49,7 +49,9 @@ echo compiler flags: %BUILD_COMPILE_FLAGS%
 echo linking flags: %BUILD_LINKING_FLAGS%
 echo ----------------------------------------
 
-REM Compilation
+REM Game binary building
+
+REM Game binary compilation
 %TOOLCHAIN_PATH%\%C_COMPILER% -c src\core\main.c -I%LIBSDL_PATH%\include\SDL2 -I%LIBSDLTTF_PATH%\include\SDL2 -Isrc\include %BUILD_COMPILE_FLAGS% -Wall -Wextra -Werror -std=c17 -o build\%BUILD_MODE%\main.o
 %TOOLCHAIN_PATH%\%C_COMPILER% -c src\utils\log\log.c -I%LIBSDL_PATH%\include\SDL2 -I%LIBSDLTTF_PATH%\include\SDL2 -Isrc\include %BUILD_COMPILE_FLAGS% -Wall -Wextra -Werror -std=c17 -o build\%BUILD_MODE%\log.o
 %TOOLCHAIN_PATH%\%C_COMPILER% -c src\core\input\events.c -I%LIBSDL_PATH%\include\SDL2 -I%LIBSDLTTF_PATH%\include\SDL2 -Isrc\include %BUILD_COMPILE_FLAGS% -Wall -Wextra -Werror -std=c17 -o build\%BUILD_MODE%\events.o
@@ -57,14 +59,32 @@ REM Compilation
 %TOOLCHAIN_PATH%\%C_COMPILER% -c src\core\video\video.c -I%LIBSDL_PATH%\include\SDL2 -I%LIBSDLTTF_PATH%\include\SDL2 -Isrc\include %BUILD_COMPILE_FLAGS% -Wall -Wextra -Werror -std=c17 -o build\%BUILD_MODE%\video.o
 %TOOLCHAIN_PATH%\%C_COMPILER% -c src\core\video\window.c -I%LIBSDL_PATH%\include\SDL2 -I%LIBSDLTTF_PATH%\include\SDL2 -Isrc\include %BUILD_COMPILE_FLAGS% -Wall -Wextra -Werror -std=c17 -o build\%BUILD_MODE%\window.o
 
-REM Linking
+REM Game binary linking
 %TOOLCHAIN_PATH%\%C_COMPILER% build\%BUILD_MODE%\main.o build\%BUILD_MODE%\log.o build\%BUILD_MODE%\events.o build\%BUILD_MODE%\keyboard.o build\%BUILD_MODE%\video.o build\%BUILD_MODE%\window.o -L%LIBSDL_PATH%\lib -L%LIBSDLTTF_PATH%\lib %BUILD_LINKING_FLAGS% -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -o build\%BUILD_MODE%\brenoGame.exe
+
+REM Tests build cleanup
+rmdir /S /Q build\tests
+mkdir build\tests
+
+REM Tests building
+
+REM Tests compilation
+set TEST_COMPILATION_PARAMS=-g -Wall -Wextra -Werror -Wno-implicit-function-declaration -std=c17
+%TOOLCHAIN_PATH%\%C_COMPILER% -c munit\munit.c -Imunit %TEST_COMPILATION_PARAMS% -o build\tests\munit.o
+%TOOLCHAIN_PATH%\%C_COMPILER% -c tests\main.c -Imunit %TEST_COMPILATION_PARAMS% -o build\tests\main.o
+%TOOLCHAIN_PATH%\%C_COMPILER% -c tests\core\video\window_tests.c -Imunit %TEST_COMPILATION_PARAMS% -o build\tests\window_tests.o
+
+REM Tests linking
+%TOOLCHAIN_PATH%\%C_COMPILER% build\tests\main.o build\tests\munit.o build\tests\window_tests.o -L%LIBSDL_PATH%\lib -L%LIBSDLTTF_PATH%\lib -mconsole -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -o build\tests\tests.exe
 
 REM Installation cleanup
 rmdir /S /Q install\%BUILD_MODE%
 mkdir install\%BUILD_MODE%
 mkdir install\%BUILD_MODE%\res
 mkdir install\%BUILD_MODE%\res\fonts
+
+rmdir /S /Q install\tests
+mkdir install\tests
 
 REM Installing
 REM Moving build binary and needed libraries
@@ -74,3 +94,6 @@ copy /B /Y build\%BUILD_MODE%\brenoGame.exe install\%BUILD_MODE%
 
 REM Moving resources files
 copy /B /Y resources\fonts\*.ttf install\%BUILD_MODE%\res\fonts
+
+REM Moving test binary
+copy /B /Y build\tests\tests.exe install\tests
