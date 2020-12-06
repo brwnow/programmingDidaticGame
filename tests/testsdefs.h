ea9452217a9e52c6535ae5d3cc79b43e89b1e0a4
Bruno Garcia <brwnow@gmail.com>
2020-12-06 17:01:29 -0300

    Adding a header of useful definitions for the tests
    For while testsdefs.h has only a wrapper for declaring a test function. It's quite useful to avoid writing a lot fo repeated code for tests functions

---
+++ tests/testsdefs.h
@@ -0,0 +1,6 @@
+#ifndef _DIDATIC_GAME_TESTS_DEFS_H
+#define _DIDATIC_GAME_TESTS_DEFS_H
+
+#define MUNIT_DECLARE_TEST_FUNC(x) static MunitResult x(const MunitParameter params[] MUNIT_UNUSED, void* user_data_or_fixture MUNIT_UNUSED)
+
+#endif // _DIDATIC_GAME_TESTS_DEFS_H
