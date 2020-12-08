8f32b39b9417ed08d31fe4fdab73232ce6af7220
Bruno Garcia <brwnow@gmail.com>
2020-12-08 03:32:06 -0300

    Implementing tests for listGetElementsCount function

--- tests/utils/datastructure/list_tests.c
+++ tests/utils/datastructure/list_tests.c
@@ -7,6 +7,18 @@
 
 MUNIT_DECLARE_TEST_FUNC(listCreate);
 
+// GETTING INFO
+
+// Getting number of elements from empty list
+MUNIT_DECLARE_SETUP_FUNC(listGetElementsCountEmpty);
+MUNIT_DECLARE_TEARDOWN_FUNC(listGetElementsCountEmpty);
+MUNIT_DECLARE_TEST_FUNC(listGetElementsCountEmpty);
+
+// Getting number of elements from populated list
+MUNIT_DECLARE_SETUP_FUNC(listGetElementsCount);
+MUNIT_DECLARE_TEARDOWN_FUNC(listGetElementsCount);
+MUNIT_DECLARE_TEST_FUNC(listGetElementsCount);
+
 // INSERTING
 
 // Inserting with push front operation in an empty list
@@ -62,6 +74,25 @@ static MunitTest listTests[] = {
         MUNIT_TEST_FUNC_NAME(listCreate),
         NULL,
         NULL,
+        MUNIT_TEST_OPTION_NONE,
+        NULL
+    },
+
+// GETTING INFO
+
+    {
+        "/listGetElementsCount-emptyList",
+        MUNIT_TEST_FUNC_NAME(listGetElementsCountEmpty),
+        MUNIT_SETUP_FUNC_NAME(listGetElementsCountEmpty),
+        MUNIT_TEARDOWN_FUNC_NAME(listGetElementsCountEmpty),
+        MUNIT_TEST_OPTION_NONE,
+        NULL
+    },
+    {
+        "/listGetElementsCount-populatedList",
+        MUNIT_TEST_FUNC_NAME(listGetElementsCount),
+        MUNIT_SETUP_FUNC_NAME(listGetElementsCount),
+        MUNIT_TEARDOWN_FUNC_NAME(listGetElementsCount),
         MUNIT_TEST_OPTION_NONE,
         NULL
     },
@@ -220,6 +251,50 @@ MUNIT_DECLARE_TEST_FUNC(listCreate) {
     munit_assert_ulong(listGetElementsCount(list), ==, 0UL);
 
     listDestroy(list);
+
+    return MUNIT_OK;
+}
+
+// GETTING INFO
+
+MUNIT_DECLARE_SETUP_FUNC(listGetElementsCountEmpty) {
+    return listCreate();
+}
+
+MUNIT_DECLARE_TEARDOWN_FUNC(listGetElementsCountEmpty) {
+    listDestroy((List*)fixture);
+}
+
+MUNIT_DECLARE_TEST_FUNC(listGetElementsCountEmpty) {
+    List *list = (List*)user_data_or_fixture;
+
+    munit_assert_not_null(list);
+    munit_assert_ulong(listGetElementsCount(list), ==, 0UL);
+
+    return MUNIT_OK;
+}
+
+// ============
+
+MUNIT_DECLARE_SETUP_FUNC(listGetElementsCount) {
+    List *list = listCreate();
+
+    listPushBack(list, malloc(sizeof(float)));
+    listPushBack(list, malloc(sizeof(float)));
+    listPushBack(list, malloc(sizeof(float)));
+
+    return list;
+}
+
+MUNIT_DECLARE_TEARDOWN_FUNC(listGetElementsCount) {
+    listDestroy((List*)fixture);
+}
+
+MUNIT_DECLARE_TEST_FUNC(listGetElementsCount) {
+    List *list = (List*)user_data_or_fixture;
+
+    munit_assert_not_null(list);
+    munit_assert_ulong(listGetElementsCount(list), ==, 3UL);
 
     return MUNIT_OK;
 }
