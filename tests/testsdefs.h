64db2e21fbc92c9c6cffcf36d8b6041a2bcc2f39
Bruno Garcia <brwnow@gmail.com>
2020-12-14 03:15:56 -0300

    Refactoring testdefs using macros for test functions names in macros for test functions declarations

--- tests/testsdefs.h
+++ tests/testsdefs.h
@@ -1,12 +1,16 @@
 #ifndef _DIDATIC_GAME_TESTS_DEFS_H
 #define _DIDATIC_GAME_TESTS_DEFS_H
 
-#define MUNIT_DECLARE_SETUP_FUNC(funcName) static void* funcName ## _setup(const MunitParameter params[] MUNIT_UNUSED, void* user_data MUNIT_UNUSED)
-#define MUNIT_DECLARE_TEARDOWN_FUNC(funcName) static void funcName ## _teardown(void* fixture)
-#define MUNIT_DECLARE_TEST_FUNC(funcName) static MunitResult funcName ## _test(const MunitParameter params[] MUNIT_UNUSED, void* user_data_or_fixture MUNIT_UNUSED)
-
-#define MUNIT_TEST_FUNC_NAME(funcName) funcName ## _test
 #define MUNIT_SETUP_FUNC_NAME(funcName) funcName ## _setup
 #define MUNIT_TEARDOWN_FUNC_NAME(funcName) funcName ## _teardown
+#define MUNIT_TEST_FUNC_NAME(funcName) funcName ## _test
+
+#define MUNIT_DECLARE_SETUP_FUNC(funcName) static void* MUNIT_SETUP_FUNC_NAME(funcName)( \
+                                                            const MunitParameter params[] MUNIT_UNUSED, \
+                                                            void* user_data MUNIT_UNUSED )
+#define MUNIT_DECLARE_TEARDOWN_FUNC(funcName) static void MUNIT_TEARDOWN_FUNC_NAME(funcName)(void* fixture)
+#define MUNIT_DECLARE_TEST_FUNC(funcName) static MunitResult MUNIT_TEST_FUNC_NAME(funcName)( \
+                                                                const MunitParameter params[] MUNIT_UNUSED, \
+                                                                void* user_data_or_fixture MUNIT_UNUSED )
 
 #endif // _DIDATIC_GAME_TESTS_DEFS_H
