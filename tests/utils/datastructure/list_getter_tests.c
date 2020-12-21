629d902f8c57ecc1d45de7316344fa905960500c
Bruno Garcia <brwnow@gmail.com>
2020-12-21 01:22:36 -0300

    Adding more tests for list getters
    Adding test for getting amount of elements in a list with few elements and a list with large amount of elements
    
    Also adding the use of fixture macros and including missing headers

--- tests/utils/datastructure/list_getter_tests.c
+++ tests/utils/datastructure/list_getter_tests.c
@@ -1,6 +1,7 @@
 #include <testsdefs.h>
 
 #include "utils/datastructure/list.h"
+#include "utils/datastructure/list_private.h"
 #include "utils/datastructure/list_tests.h"
 #include "utils/datastructure/test_list_setups.h"
 
@@ -11,7 +12,7 @@ DEFINE_STANDALONE_TEST_FUNC(listGetElementsCountNullPtr) {
 }
 
 DEFINE_FULL_TEST_FUNC(listGetElementsCountEmpty, listEmpty) {
-    List *list = (List*)user_data_or_fixture;
+    List *list = user_data_or_fixture;
 
     munit_assert_not_null(list);
     munit_assert_ulong(listGetElementsCount(list), ==, 0UL);
@@ -20,10 +21,30 @@ DEFINE_FULL_TEST_FUNC(listGetElementsCountEmpty, listEmpty) {
 }
 
 DEFINE_FULL_TEST_FUNC(listGetElementsCountSingleElement, listSingleElement) {
-    List *list = (List*)user_data_or_fixture;
+    List *list = FIXTURE_INDEX(user_data_or_fixture, 0);
 
     munit_assert_not_null(list);
     munit_assert_ulong(listGetElementsCount(list), ==, 1UL);
+
+    return MUNIT_OK;
+}
+
+DEFINE_FULL_TEST_FUNC(listGetElementsCountFewElements, listFewElements) {
+    List *list = FIXTURE_INDEX(user_data_or_fixture, 0);
+    const size_t numberOfElements = *(size_t*)FIXTURE_INDEX(user_data_or_fixture, 2);
+
+    munit_assert_not_null(list);
+    munit_assert_ulong(listGetElementsCount(list), ==, numberOfElements);
+
+    return MUNIT_OK;
+}
+
+DEFINE_FULL_TEST_FUNC(listGetElementsCountLargeAmountRandom, listLargeAmountElementsRandomValue) {
+    List *list = FIXTURE_INDEX(user_data_or_fixture, 0);
+    const size_t numberOfElements = *(size_t*)FIXTURE_INDEX(user_data_or_fixture, 2);
+
+    munit_assert_not_null(list);
+    munit_assert_ulong(listGetElementsCount(list), ==, numberOfElements);
 
     return MUNIT_OK;
 }
@@ -36,6 +57,8 @@ static MunitTest listTests[] = {
     GET_TEST_FUNC_ARRAY_ENTRY("/listGetElementsCount-nullPtr", listGetElementsCountNullPtr),
     GET_TEST_FUNC_ARRAY_ENTRY("/listGetElementsCount-emptyList", listGetElementsCountEmpty),
     GET_TEST_FUNC_ARRAY_ENTRY("/listGetElementsCount-singleElement", listGetElementsCountSingleElement),
+    GET_TEST_FUNC_ARRAY_ENTRY("/listGetElementsCount-fewElements", listGetElementsCountFewElements),
+    GET_TEST_FUNC_ARRAY_ENTRY("/listGetElementsCount-largeAmountRandom", listGetElementsCountLargeAmountRandom),
 
     // Ending of tests array
     { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
