92ffc96030d2c520345106a5656342d235b7caf1
Bruno Garcia <brwnow@gmail.com>
2020-12-26 19:57:51 -0300

    Adding tests for static creators in list module

--- tests/utils/datastructure/list_create_tests.c
+++ tests/utils/datastructure/list_create_tests.c
@@ -5,6 +5,56 @@
 #include "utils/datastructure/list_tests.h"
 #include "utils/datastructure/test_list_setups.h"
 
+DEFINE_STANDALONE_TEST_FUNC(createNode) {
+    Node *node = createNode((void*)1UL,
+                            (Node*)2UL,
+                            (Node*)3UL,
+                            (List*)4UL);
+
+    munit_assert_not_null(node);
+    munit_assert_ptr_equal(node->data, (void*)1UL);
+    munit_assert_ptr_equal(node->prev, (void*)2UL);
+    munit_assert_ptr_equal(node->next, (void*)3UL);
+    munit_assert_ptr_equal(node->parent, (void*)4UL);
+
+    destroyNode(node);
+
+    return MUNIT_OK;
+}
+
+// ============
+
+DEFINE_STANDALONE_TEST_FUNC(createNodeNullParent) {
+    Node *node = createNode((void*)1UL,
+                            (Node*)2UL,
+                            (Node*)3UL,
+                            NULL);
+
+    munit_assert_null(node);
+
+    // For the case it failed and didn't returned null
+    destroyNode(node);
+
+    return MUNIT_OK;
+}
+
+// ============
+
+DEFINE_STANDALONE_TEST_FUNC(createIterator) {
+    ListIterator *iterator = createIterator((void*)1UL, (Node*)2UL);
+
+    munit_assert_not_null(iterator);
+    munit_assert_ptr_equal(iterator->data, (void*)1UL);
+    munit_assert_ptr_equal(iterator->currentNode, (void*)2UL);
+
+    // For the case it failed and didn't returned null
+    free(iterator);
+
+    return MUNIT_OK;
+}
+
+// ============
+
 DEFINE_STANDALONE_TEST_FUNC(listCreate) {
     List *list = listCreate();
 
@@ -23,6 +73,9 @@ DEFINE_STANDALONE_TEST_FUNC(listCreate) {
 // =================
 
 static MunitTest listTests[] = {
+    GET_TEST_FUNC_ARRAY_ENTRY("/createNode", createNode),
+    GET_TEST_FUNC_ARRAY_ENTRY("/createNode-nullParent", createNodeNullParent),
+    GET_TEST_FUNC_ARRAY_ENTRY("/createIterator", createIterator),
     GET_TEST_FUNC_ARRAY_ENTRY("/listCreate", listCreate),
 
     TEST_FUNC_ARRAY_END
